df4454591ea3e7112d924ca18b4dc89a868f6ccd
rocco <rocco.reus@hva.nl>
2024-04-17 19:47:44 +0200

    feat: add default springboot project

---
+++ src/backend/src/main/java/caaruujuuwoo65/backend/HomeController.java
@@ -0,0 +1,15 @@
+package caaruujuuwoo65.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the Spring Boot Application!");
+        return "home";
+    }
+}

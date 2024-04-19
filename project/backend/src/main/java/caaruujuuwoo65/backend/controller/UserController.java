5749d8717d0ee05f39cbdad7901bf117641f3a8b
Aaron laan <aaron.laan@gmail.com>
2024-04-19 09:52:57 +0200

    Added: rest controller, packages for config and controller. Edited: temporary removal of security.

---
+++ project/backend/src/main/java/caaruujuuwoo65/backend/controller/UserController.java
@@ -0,0 +1,15 @@
+package caaruujuuwoo65.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/")
+    public String helloworld() {
+        return "Hello, world!";
+    }
+}

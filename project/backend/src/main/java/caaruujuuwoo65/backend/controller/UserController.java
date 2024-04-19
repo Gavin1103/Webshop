38c6c1dffc660f4314504fb329f8c41ffb249d41
Aaron laan <aaron.laan@gmail.com>
2024-04-19 14:16:14 +0200

    Implemented: swagger ui.

--- project/backend/src/main/java/caaruujuuwoo65/backend/controller/UserController.java
+++ project/backend/src/main/java/caaruujuuwoo65/backend/controller/UserController.java
@@ -1,7 +1,10 @@
 package caaruujuuwoo65.backend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/")
+    @Operation(summary = "Hello world.")
+    @ResponseStatus(HttpStatus.OK)
     public String helloworld() {
         return "Hello, world!";
     }

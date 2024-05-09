3e14373601fd4fa3454580d332478e41c6a32527
Gavin <gtjin1103@gmail.com>
2024-05-09 02:06:47 +0200

    Added: repositories

---
+++ project/backend/src/main/java/caaruujuuwoo65/backend/repository/OrderedProductsRepository.java
@@ -0,0 +1,11 @@
+package caaruujuuwoo65.backend.repository;
+
+import caaruujuuwoo65.backend.model.OrderedProducts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderedProductsProductRepository extends JpaRepository<OrderedProducts, Long> {
+
+}

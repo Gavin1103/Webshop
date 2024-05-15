e33ca2aeccfe05765e3a3c30218d52da55854d58
Gavin <gtjin1103@gmail.com>
2024-05-15 18:18:49 +0200

    feature: ready to begin styling product detail page

--- project/frontend/src/components/views/product-detail-page.ts
+++ project/frontend/src/components/views/product-detail-page.ts
@@ -2,6 +2,7 @@ import { html, LitElement, TemplateResult } from "lit";
 import { customElement } from "lit/decorators.js";
 import { Product } from "../../../types/Product";
 import { ProductService } from "../../services/ProductService";
+import { Router } from "@vaadin/router";
 
 
 @customElement("product-detail-page")
@@ -10,15 +11,16 @@ export class ProductDetailPage extends LitElement {
     private productService: ProductService = new ProductService();
     private fakeProductId: number = 1;
 
-    private async getProductById(): Promise<void> {
-
-        const product: Product | undefined = await this.productService.getProductById(this.fakeProductId);
-        console.log(typeof product);
-    }
 
     public async render(): Promise<TemplateResult> {
 
-        await this.getProductById();
+        const product: Product | undefined = await this.productService.getProductById(this.fakeProductId);
+
+        if(!product){
+            Router.go("/product-not-found");
+        }
+
+        console.log(product);
 
         return html`
             <div>
@@ -29,6 +31,7 @@ export class ProductDetailPage extends LitElement {
                 <br>
                 <br>
                 <h2>Product detail page</h2>
+
             </div>
         `;
 

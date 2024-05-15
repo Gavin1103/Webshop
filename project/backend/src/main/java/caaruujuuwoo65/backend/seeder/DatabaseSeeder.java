944f3ad71d2ff16c451f1a98a8c9a8ab797f109b
Aaron laan <aaron.laan@gmail.com>
2024-05-15 12:08:50 +0200

    Added: seeding

---
+++ project/backend/src/main/java/caaruujuuwoo65/backend/seeder/DatabaseSeeder.java
@@ -0,0 +1,44 @@
+package caaruujuuwoo65.backend.seeder;
+
+import caaruujuuwoo65.backend.model.User;
+import caaruujuuwoo65.backend.model.enums.Role;
+import caaruujuuwoo65.backend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Component
+@Order(1)
+public class DatabaseSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public DatabaseSeeder(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Create admin user
+        User admin = new User();
+        admin.setEmail("admin@admin.com");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setRoles(new HashSet<>(Set.of(Role.ADMIN)));
+        this.userRepository.save(admin);
+
+        // Create normal user
+        User user = new User();
+        user.setEmail("user@user.com");
+        user.setPassword(passwordEncoder.encode("user"));
+        admin.setRoles(new HashSet<>(Set.of(Role.USER)));
+        this.userRepository.save(user);
+    }
+}

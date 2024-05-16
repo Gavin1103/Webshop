d04d7e7a4cedcae87cac2365ebe725873af9575e
Aaron laan <aaron.laan@gmail.com>
2024-05-16 09:09:22 +0200

    Added: validation decorators to DTO. TODO: Add config to ensure they work.

--- project/backend/src/main/java/caaruujuuwoo65/backend/dto/UserDto.java
+++ project/backend/src/main/java/caaruujuuwoo65/backend/dto/UserDto.java
@@ -1,5 +1,8 @@
 package caaruujuuwoo65.backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +13,22 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 public class UserDto {
+    @NotBlank(message = "Username is mandatory")
     private String username;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotBlank(message = "Phone number is mandatory")
     private String phoneNumber;
+
+    @NotBlank(message = "First name is mandatory")
     private String firstname;
+
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String lastname;
+
+    @NotBlank(message = "Password is mandatory")
     private String password;
 }

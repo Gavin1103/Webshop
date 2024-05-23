fe1fb1c09474e5191127ae4a08455de1f21ddc3c
rocco <rocco.reus@hva.nl>
2024-05-23 22:57:47 +0200

    added proxy for screenshot server

--- project/proxy/src/index.ts
+++ project/proxy/src/index.ts
@@ -18,6 +18,20 @@ app.use('/springboot', proxy('http://oege.ie.hva.nl:8069', {
     }
 }));
 
+app.use('/screenshot', proxy('http://oege.ie.hva.nl:8070', {
+    proxyReqPathResolver: (req: { originalUrl: string; }) => {
+        const newPath = req.originalUrl.replace('/api/screenshot', '/screenshot');
+        console.log(`Proxying request to: ${newPath}`);
+        return newPath;
+    },
+    proxyErrorHandler: (err: any, res: {
+        status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; };
+    }, _next: any) => {
+        console.error('Proxy error:', err);
+        res.status(500).send('Proxy error');
+    }
+}));
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`CORS Proxy running on http://localhost:${port}`);

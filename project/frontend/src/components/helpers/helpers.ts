644d5fe0a798f96ba3b84069dd6b3f0ebf1ac8b5
Rocco Reus <rocco.reus@hva.nl>
2024-05-29 11:36:59 +0200

    fix: removed script

--- project/frontend/src/components/helpers/helpers.ts
+++ project/frontend/src/components/helpers/helpers.ts
@@ -1,3 +1,16 @@
 export function roundToTwoDecimals(number: number): string {
     return (Math.round(number * 100) / 100).toFixed(2);
 }
+
+export function getCookie(name: string): string | null {
+    const nameLenPlus: number = (name.length + 1);
+    return document.cookie
+        .split(';')
+        .map(c => c.trim())
+        .filter(cookie => {
+            return cookie.substring(0, nameLenPlus) === `${name}=`;
+        })
+        .map(cookie => {
+            return decodeURIComponent(cookie.substring(nameLenPlus));
+        })[0] || null;
+}
